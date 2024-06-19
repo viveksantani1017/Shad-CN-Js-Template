8a5b81453c828392cd7dd675d288a5b8ed14853e
Vivek Santani <80535903+viveksantani1017@users.noreply.github.com>
2024-06-19 18:06:09 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <Button>Hello</Button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
